6d41717449daa5a5e4bbd72b14f475176634a209
Guilherme <guilhermecorsino21@gmail.com>
2023-03-06 21:43:12 -0300

    finalizando a estilização, faltando ajeitar o footer do comentário

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, TrashSimple } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment(){
+    return(
+        <div className={styles.comment}>
+            <img src='https://github.com/dmalberto.png' alt=""/>
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Diego Alberto</strong>
+                            <time title='25 de Fevereiro às 13:25h' dateTime="2023-02-25 13:25:40">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deleter comentário'>
+                            <TrashSimple size={22}/>
+                        </button>
+                        
+                    </header>
+
+                    <p>Muito bom, Parabéns!!</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp/>
+                        Curtir <span>70</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+
+}
